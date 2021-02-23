958a166a30bf3990ebd9a9cf435f22a69b5d9cbc
abi-hong <flffjakfwm20@gmail.com>
2021-02-23 22:25:28 +0900

    redux login page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,18 +1,21 @@
 import React,{ useEffect} from 'react';
 import axios from 'axios';
-
-function LandingPage() {
+import { withRouter } from 'react-router-dom';
+function LandingPage(props) {
 
     useEffect(() => {
         axios.get('/api/hello')
-        .then(response => console.log(response.data))
+        .then(response => {console.log(response)})
     }, [])
 
     return (
-        <div>
-            LandingPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+        }}>
+            <h2>시작페이지</h2>
         </div>
     )
 }
 
-export default LandingPage
+export default withRouter(LandingPage)
