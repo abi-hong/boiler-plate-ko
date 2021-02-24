67c4b56efd5e78ca306a84b4cbab0369ae4bc6ef
abi-hong <flffjakfwm20@gmail.com>
2021-02-24 20:21:39 +0900

    register logout auth

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
@@ -24,6 +24,10 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/api/hello', (req,res) => {
+  res.send("안녕하세요~!!")
 })
 
 app.post('/api/users/register', (req, res) => {
@@ -92,6 +96,8 @@ app.get('/api/users/logout', auth, (req, res) => {
       })
     })
 })
+
+const port = 5000
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
